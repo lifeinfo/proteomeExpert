663f1dad6591378de9142625dfed7f661b3f3926
lifeinfo <zhutiansheng@wias.org.cn>
2019-11-18 13:06:28 +0800

    add batch deisgn check

--- expert/DataInputCheck.R
+++ expert/DataInputCheck.R
@@ -17,3 +17,13 @@ technical_file_check<-function(file,sep,header){
 batch_file_check<-function(file,sep,header){
   
 }
+batch_design_check<-function(file,sep,header){
+  errors<-NULL
+  d<-read.table(file,sep = sep,header = header)
+  if(ncol(d)==1){
+    errors<-"Please select correct separator for your file. One sample is not allowed"
+  }else if(nrow(d)!=length(unique(d[,1]))){
+    errors<-"Duplicated values in first column is not allowed "
+  }
+  return(errors)
+}
